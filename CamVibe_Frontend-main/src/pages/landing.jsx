7ef69f16c0868ceec620612778e5b51aa1144fba
kartu010704 <kartavyaparhate010704@gmail.com>
2024-07-22 17:02:39 +0530

    first commit

---
+++ CamVibe_Frontend-main/src/pages/landing.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import "../App.css"
+
+import { Link, useNavigate } from 'react-router-dom'
+
+export default function LandingPage() {
+
+
+    const router = useNavigate();
+
+    return (
+        <div className='landingPageContainer'>
+            <nav>
+                <div className='navHeader'>
+                    <h2>CamVibe<i class="fa-solid fa-video"></i></h2>
+                </div>
+                <div className='navlist'>
+                    <p onClick={() => {
+                        router("/aljk23")
+                    }}>Join as Guest</p>
+                    <p onClick={() => {
+                        router("/auth")
+
+                    }}>Register</p>
+                    <div onClick={() => {
+                        router("/auth")
+
+                    }} role='button'>
+                        <p>Login</p>
+                    </div>
+                </div>
+            </nav>
+
+
+            <div className="landingMainContainer">
+                <div>
+                    <h1><span style={{ color: "#FF9839" }}>Connect</span> with your loved Ones</h1>
+
+                    <p>Cover a distance by CamVibe</p>
+                    <div role='button'>
+                        <Link to={"/auth"}>Get Started</Link>
+                    </div>
+                </div>
+                <div>
+
+                    <img src="/mobile.png" alt="" />
+
+                </div>
+            </div>
+
+
+
+        </div>
+    )
+}
